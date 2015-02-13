7ffbdbc429956a6f190c762da0626d1b6508f1d0
marcinkuptel <marcinkuptel@gmail.com>
2015-02-13 15:47:04 +0100

    Creating the project

---
+++ FitToday/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FitToday
+//
+//  Created by Marcin Kuptel on 13/02/15.
+//  Copyright (c) 2015 Marcin Kuptel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
